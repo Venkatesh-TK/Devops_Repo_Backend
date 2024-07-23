81bd3b7fa3e31fbf18b1ec44885e630b02594e8f
Venkatesh-TK <114131840+Venkatesh-TK@users.noreply.github.com>
2024-07-23 16:08:03 +0530

    controller added

---
+++ Devops-Backend/src/main/java/com/backend/TestController.java
@@ -0,0 +1,17 @@
+package com.backend;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<Object> getWelcome(){
+        return new ResponseEntity<>("Welcome to DevOps Training...", HttpStatus.OK);
+    }
+}
